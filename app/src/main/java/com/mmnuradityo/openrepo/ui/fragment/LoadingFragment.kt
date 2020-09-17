6f5eb7d4772332db1070a2170705da442f574cb0
mmnuradityo <nuradityo@gmail.com>
2020-09-17 11:10:39 +0700

    improved ui

--- app/src/main/java/com/mmnuradityo/openrepo/ui/fragment/LoadingFragment.kt
+++ app/src/main/java/com/mmnuradityo/openrepo/ui/fragment/LoadingFragment.kt
@@ -1,22 +1,22 @@
 package com.mmnuradityo.openrepo.ui.fragment
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.mmnuradityo.openrepo.R
-import kotlinx.android.synthetic.main.fragment_loading.*
+import com.mmnuradityo.openrepo.base.BaseFragment
+import com.mmnuradityo.openrepo.databinding.FragmentLoadingBinding
 
 /**
  * Created on : 01/09/20
  * Author     : mmnuradityo
  * GitHub     : https://github.com/mmnuradityo
  */
-class LoadingFragment : Fragment() {
+class LoadingFragment : BaseFragment<LoadingFragment>() {
 
     companion object {
         private const val DATA_KEY = "data_key"
@@ -30,39 +30,51 @@ class LoadingFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
+    private lateinit var viewBinding: FragmentLoadingBinding
+
+    override fun initComponent(context: Context) {
+    }
+
+    override fun initViewComponent(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_loading, container, false)
+    ): View? {
+        viewBinding = FragmentLoadingBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
 
+    override fun initView(view: View, savedInstanceState: Bundle?) {
+        with(viewBinding) {
+            val handler = Handler(Looper.getMainLooper())
+            try {
+                val runnable: Runnable = object : Runnable {
+                    var count = 0
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val handler = Handler(Looper.getMainLooper())
-        try {
-            val runnable: Runnable = object : Runnable {
-                var count = 0
-                @SuppressLint("SetTextI18n")
-                override fun run() {
-                    count++
-                    tvLoading?.text = when (count) {
-                        1 -> "Loading . "
-                        2 -> "Loading . . "
-                        3 -> "Loading . . ."
-                        else -> "Loading "
+                    @SuppressLint("SetTextI18n")
+                    override fun run() {
+                        count++
+                        textLoading = when (count) {
+                            1 -> "Loading . "
+                            2 -> "Loading . . "
+                            3 -> "Loading . . ."
+                            else -> "Loading "
+                        }
+                        if (count == 4) count = 0
+                        handler.postDelayed(this, 2 * 300.toLong())
                     }
-                    if (count == 4) count = 0
-                    handler.postDelayed(this, 2 * 300.toLong())
                 }
+
+                handler.postDelayed(runnable, 1 * 300.toLong())
+
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
 
-            handler.postDelayed(runnable, 1 * 300.toLong())
-
-        } catch (e: Exception) {
-            e.printStackTrace()
+            executePendingBindings()
         }
+    }
 
+    override fun listener() {
     }
 }
